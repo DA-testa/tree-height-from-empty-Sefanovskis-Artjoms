aad8117f4a747da57149bc6bc84c8eba99a031d6
Sefanovskis-Artjoms <artjoms200@gmail.com>
2023-03-03 13:12:16 +0200

    return to previeous state

--- tree_height.py
+++ tree_height.py
@@ -7,10 +7,11 @@ import threading
 import numpy as np
 
 
-def compute_height(n, nodes):
+def compute_height(n, parents):
     # Write this function
     max_height = 0
     # Your code here
+    nodes = np.array(list(map(int,parents.split(" "))))
     currentNode = np.where(nodes == -1)[0]
     parentNodes = []
     visited = []
@@ -44,14 +45,13 @@ def main():
             return
         with open("test/" + fileName, "r") as f:
             nodeCount = int(f.readline())
-            nodes = np.array(list(map(int,f.readline.split(" "))))
-            
+            nodes = f.readline
     # account for github input inprecision
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     elif "I" in inpMethod:
         nodeCount = input()
-        nodes = np.array(list(map(int,input().split(" "))))
+        nodes = input()
     # call the function and output it's result
     height = compute_height(nodeCount,nodes)
     print(height)
